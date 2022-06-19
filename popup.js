09c57877b5ce07d3e1dd99ad20f0f0b11c52d160
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-06-19 13:30:32 -0300

    welcome

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
 const hamburguer = document.querySelector(".hamburguer");
 const menu = document.querySelector("#nav-menu");
+const welcome = document.querySelector(".welcome")
 
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("active");
     menu.classList.toggle("active");
+    welcome.classList.toggle("inactive");
 })
 
 document.querySelectAll(".nav-link").forEach(n => m.addEventListener("click", () => {
