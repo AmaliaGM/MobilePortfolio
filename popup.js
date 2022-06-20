a19437afdf03b7e8bfe87537b6055a2d5570bc50
Tiago Marin <tiago.lelinski@gmail.com>
2022-06-20 13:36:18 -0300

    add styling to mobile menu

--- popup.js
+++ popup.js
@@ -1,14 +1,17 @@
 const hamburguer = document.querySelector(".hamburguer");
 const menu = document.querySelector("#nav-menu");
-const welcome = document.querySelector(".welcome")
+const welcome = document.querySelector(".welcome");
+const body = document.body;
 
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("active");
     menu.classList.toggle("active");
     welcome.classList.toggle("inactive");
+    body.classList.toggle("stopscroll");
 })
 
 document.querySelectAll(".nav-link").forEach(n => m.addEventListener("click", () => {
     hamburguer.classList.remove("active");
-    menu.classList.remove("active")
+    menu.classList.remove("active");
+    body.classList.remove("stopscroll");
 })) 
