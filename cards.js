ad10e8650a66a0b889fde08dd6f606f2c072f695
Tiago Marin <tiago.lelinski@gmail.com>
2022-06-21 21:25:13 -0300

    add js file with a database

--- cards.js
+++ cards.js
@@ -1,3 +1,10 @@
-const  = [
-  {}
-]
+const cards= [
+  { 
+    image: 'images/plan.png',
+    heading:'1 Multi-Post Stories',
+    badge1: 'Ruby on Rails',
+    badge2: 'css',
+    badge3: 'Java Script',
+    badge4: 'HTML',
+  },
+];
