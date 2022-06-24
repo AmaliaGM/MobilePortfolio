8728af634d9ff11e559d67fb6c6b48eac6cfb38d
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-06-24 16:01:49 -0300

    button gap

--- popup.js
+++ popup.js
@@ -113,7 +113,7 @@ liLang6D.innerText = "Codepen"
 ulLang.appendChild(liLang6D);
 
 let loremIp = document.createElement("p");
-loremIp.classList = "lorem";
+loremIp.classList = "loremText";
 loremIp.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea"
 popup1.appendChild(loremIp);
 
